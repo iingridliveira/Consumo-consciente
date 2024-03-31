1664de30de06314d287ba772b5acc38c66a3b40f
Ingrid <oligrid20@gmail.com>
2024-03-31 17:17:09 -0300

    feat: new cors

--- Backend/server.js
+++ Backend/server.js
@@ -7,7 +7,7 @@ import { Routes } from "./src/router/index.routes.js";
 dotenv.config();
 const app = express();
 const port = process.env.PORT;
-app.use(cors())
+app.use(cors({origin: "*"}))
 app.use(express.json());
 app.use(Routes);
 
