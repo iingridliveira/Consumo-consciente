1bdcb41a015c7c23298335f9eba0e0d2f1200ba5
Ingrid <oligrid20@gmail.com>
2024-04-04 09:46:17 -0300

    feat: Added a button

---
+++ Frontend/src/components/Button/Button.jsx
@@ -0,0 +1,29 @@
+import "./Button.css"
+import {Link} from "react-router-dom";
+import PropTypes from 'prop-types';
+const Button = ({about, Consume}) => {
+    return(
+        <div className={"btn"}>
+            <ul>
+                <li>
+                    <Link to={`/`}>{about}</Link>
+                </li>
+                <li>
+                    <Link to={`/Consumo`}>{Consume}</Link>
+                </li>
+            </ul>
+        </div>
+
+    )
+
+}
+Button.propTypes = {
+    about:  PropTypes.string.isRequired,
+    Consume: PropTypes.string.isRequired
+
+};
+
+
+
+export default Button
+
